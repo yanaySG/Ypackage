ce61263b8552d0972cec5f3f37c33eb373ac6d54
yanaySG <yanay.sg@gmail.com>
2017-01-24 03:28:55 +0100

    'init'

---
+++ R/dummyFunction1.R
@@ -0,0 +1,10 @@
+#' Dummy Function 1
+#'
+#' This is the Dummy Function 1
+#' @return The text 'This is a Dummy Function 1'
+#' @export
+
+dummyFunction1 <- function() {
+  print("This is a Dummy Function 1")
+}
+
---
+++ R/dummyFunction2.R
@@ -0,0 +1,10 @@
+#' Dummy Function 2
+#'
+#' This is the Dummy Function 2
+#' @return The text 'This is a Dummy Function 2'
+#' @export
+
+dummyFunction2 <- function() {
+  print("This is a Dummy Function 2")
+}
+
